36b65c30699f8ce49bfaca63273c2e823d501ca9
Rafał Kowalczyk <rako.r.kowalczyk@interia.pl>
2019-05-13 22:30:16 +0200

    Add html and style for section motto

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,23 +9,12 @@ const sidebarBox = document.querySelector('#menu-content'),
     sidebarBtn = document.querySelector('#menu-hamburger'),
     pageWrapper = document.querySelector('#hero');
 
-sidebarBtn.addEventListener('click', function () {
+sidebarBtn.addEventListener('click', function (event) {
     sidebarBtn.classList.toggle('active');
     sidebarBox.classList.toggle('active');
 });
 
-pageWrapper.addEventListener('click', function () {
-
-    if (sidebarBox.classList.contains('active')) {
-        sidebarBtn.classList.remove('active');
-        sidebarBox.classList.remove('active');
-    }
-});
-
-window.addEventListener('keydown', function (event) {
-
-    if (sidebarBox.classList.contains('active') && event.keyCode === 27) {
-        sidebarBtn.classList.remove('active');
-        sidebarBox.classList.remove('active');
-    }
+sidebarBox.addEventListener('click', function (event) {
+    sidebarBtn.classList.toggle('active');
+    sidebarBox.classList.toggle('active');
 });
